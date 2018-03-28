1cfc84735bffa135f3b92b3605c533b0d4527a90
Amlexandria <antestumejorsonrisa@gmail.com>
2018-03-28 11:45:01 -0600

    Se arregla arror en la llamada de la funcion create Player

--- js/app.js
+++ js/app.js
@@ -15,19 +15,21 @@ function addPoints(name,points){//aqui estoy ligando los puntos
 function printAllPoints(){
   var arrayPlayers = Object.keys(scores);
   var resultados = "";
-  for (var k = 0; k < arrayPlayers.length; k++){
-    if(k == arrayPlayers.length -1){//se verifica que estamos en el ultimo index
-      resultados += arrayPlayers[k] +': ' scores[arrayPlayers[k]] + ".";
-    }else {//si no estamos en el ultimo index se deja la coma
-      resultados += arrayPlayers[k] +': ' scores[arrayPlayers[k]] + ",";
+  for (var k = 0; k < arrayPlayers.length; k++) {
+        if (k == arrayPlayers.length -1) {//se verifica que estamos en el ultimo index
+      resultados += arrayPlayers[k] + ': ' + scores[arrayPlayers[k]] + ".";
+    } else {//si no estamos en el ultimo index se deja la coma
+      resultados += arrayPlayers[k] +': ' + scores[arrayPlayers[k]] + ",";
     }
+    return resultados;
   }
 
-  return resultados;
   console.log(printAllPoints());
 
 }//function printAllPoints
 
-createPlayer(scores,"name");
+createPlayer(scores,"Ana");
 addPoints ("Ana",20);
 addPoints("ana",40);
+createPlayer(scores, "Silvana");
+addPoints("Silvana", 40);
